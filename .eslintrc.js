5f2ad04d178b0091e8c445acc876e06caa47323e
Swann KNANI <swannknani@Swanns-MBP-2.paris.octo>
2017-05-12 18:23:05 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html',
+    'eslint-plugin-html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    'no-tabs': 0,
+    'padded-blocks': [2, 'always'],
+    'no-console': 0,
+    'max-len': [2, 200],
+    'indent': [2, 'tab'],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const serveStatic = require('serve-static'); //
+//
+// const app = express();
+// app.use(serveStatic(__dirname));
+// const port = process.env.PORT || 5000;
+// app.listen(port);
+//
+// console.log(`server started ${port}`);
