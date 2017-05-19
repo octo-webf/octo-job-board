a9f63f1c314caf7dd3cd370dbee96f685ba649f5
Swann KNANI <swannknani@Swanns-MBP-2.paris.octo>
2017-05-19 19:02:41 +0200

    Add homepage with google-signiun-button

---
+++ src/components/GoogleSignInComponent.vue
@@ -0,0 +1,34 @@
+<template>
+
+	<div class="">
+		<g-signin-button>rfrfrf</g-signin-button>
+
+	</div>
+
+
+</template>
+
+<script>
+
+import Vue from 'vue';
+import GSignInButton from 'vue-google-signin-button';
+
+Vue.use(GSignInButton);
+
+export default {
+
+	name: 'google-signin',
+
+	components: {
+
+		GSignInButton,
+
+	},
+
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
