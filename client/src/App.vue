ca83a977078f35497d20fe07cbcaa290896e4874
Pierre Trollé <ptrolle@octo.com>
2017-06-09 16:14:32 +0200

    feat: JOB-4 --> Display job cards in the job list

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,12 +12,21 @@ export default {
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+	body {
+		background: #f5f5f5;
+		padding: 0;
+		margin: 0;
+	}
+
+	#app {
+		font-family: "Roboto", "Helvetica Neue", Arial, sans-serif;
+	}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
