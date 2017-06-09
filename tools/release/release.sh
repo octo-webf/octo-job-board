1ad58b933bbe330382e21f5c8a5b198dbb77bc8c
Jérémy Buget <jbuget@gmail.com>
2017-06-09 19:01:56 +0200

    Add 'npm release' command

---
+++ tools/release/release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eux
+
+echo "Check for pending changes"
+if [[ $(git status --porcelain) ]]; then
+  echo "You have uncommitted changes. You can not release. Please commit or stash your changes before releasing." >&2
+  exit 1
+fi
+
+echo "Fetch and pull branch dev on origin host"
+git fetch origin dev
+git pull origin dev
+
+echo "Fetch and pull branch master on origin host"
+git fetch origin master
+git pull origin master
+
+echo "Merge and push branch dev on master"
+git checkout master
+git merge dev
+git push origin master --force
+
+echo "Go back to branch dev"
+git checkout dev
+
+exit 0
