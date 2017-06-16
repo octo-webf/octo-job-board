12b7cf1819b2429658eb94adf03c280aeb06a1cc
Jérémy Buget <jbuget@gmail.com>
2017-06-16 15:59:16 +0200

    Declare use of OctopodClient into /jobs route

--- server/src/routes/jobs.js
+++ server/src/routes/jobs.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const jobsFixtures = require('../fixtures/jobs')
 const auth = require('../middlewares/auth')
+const OctopodClient = require('../utils/octopod-client')
 
 router.get('/', auth, function (req, res, next) {
-  res.json(jobsFixtures)
+  OctopodClient.fetchJobs().then((jobs) => {
+    res.send(jobs)
+  })
 })
 
 module.exports = router
--- server/test/integration/routes/jobs.spec.js
+++ server/test/integration/routes/jobs.spec.js
@@ -2,21 +2,42 @@ const {request, expect, sinon} = require('../../test-helper')
 const app = require('../../../app')
 const jobs = require('../../../src/fixtures/jobs')
 const GoogleAuthWrapper = require('../../../src/utils/google-auth-wrapper')
+const OctopodClient = require('../../../src/utils/octopod-client')
 
 describe('Integration | Routes | jobs route', function () {
   beforeEach(() => {
     sinon.stub(GoogleAuthWrapper, 'verifyIdToken').resolves({userId: 'user-id', domain: 'octo.com'})
+    sinon.stub(OctopodClient, 'fetchJobs').resolves(jobs)
   })
 
   afterEach(() => {
     GoogleAuthWrapper.verifyIdToken.restore()
+    OctopodClient.fetchJobs.restore()
   })
 
-  it('should return fixtures jobs', (done) => {
+  it('should return 401 response if the user is not well authenticated', () => {
+    return request(app)
+      .get('/api/jobs')
+      .expect(401)
+  })
+
+  it('should call octopod client to fetch actual job data', (done) => {
     request(app)
       .get('/api/jobs')
       .set('Authorization', 'Bearer titi-toto')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        }
+        expect(OctopodClient.fetchJobs).to.have.been.called
+        done()
+      })
+  })
+
+  it('should return the formatted job data', (done) => {
+    request(app)
+      .get('/api/jobs')
+      .set('Authorization', 'Bearer titi-toto')
       .end((err, res) => {
         if (err) {
           done(err)
@@ -24,11 +45,5 @@ describe('Integration | Routes | jobs route', function () {
         expect(res.body).to.deep.equal(jobs)
         done()
       })
-  })
-
-  it('should return 401 response if the user is not well authenticated', () => {
-    return request(app)
-      .get('/api/jobs')
-      .expect(401)
   })
 })
