8b3cc32b47bf0625f7d02f866a843c31b894c747
Jérémy Buget <jbuget@gmail.com>
2017-06-20 23:13:27 +0200

    Add Istanbul code coverage (+ fix ESLint errors)

--- client/test/e2e/runner.js
+++ client/test/e2e/runner.js
@@ -1,8 +1,9 @@
 // 1. start the dev server using production config
 process.env.NODE_ENV = 'testing';
-var server = require('../../build/dev-server.js');
+const server = require('../../build/dev-server.js');
 
 server.ready.then(() => {
+
   // 2. run the nightwatch test suite against it
   // to run in additional browsers:
   //    1. add an entry in test/e2e/nightwatch.conf.json under "test_settings"
@@ -10,24 +11,33 @@ server.ready.then(() => {
   // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
   // For more information on Nightwatch's config file, see
   // http://nightwatchjs.org/guide#settings-file
-  var opts = process.argv.slice(2);
-  if (opts.indexOf('--config') === -1) {
-    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
-  }
-  if (opts.indexOf('--env') === -1) {
-    opts = opts.concat(['--env', 'chrome']);
-  }
+	let opts = process.argv.slice(2);
+	if (opts.indexOf('--config') === -1) {
 
-  var spawn = require('cross-spawn');
-  var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
+		opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
 
-  runner.on('exit', function (code) {
-    server.close();
-    process.exit(code);
-  });
+	}
+	if (opts.indexOf('--env') === -1) {
 
-  runner.on('error', function (err) {
-    server.close();
-    throw err;
-  });
+		opts = opts.concat(['--env', 'chrome']);
+
+	}
+
+	const spawn = require('cross-spawn');
+	const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
+
+	runner.on('exit', (code) => {
+
+		server.close();
+		process.exit(code);
+
+	});
+
+	runner.on('error', (err) => {
+
+		server.close();
+		throw err;
+
+	});
+
 });
