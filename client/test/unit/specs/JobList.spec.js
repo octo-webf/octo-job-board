6415e64205518d0657d70c8145ec62f362a5b56f
Jérémy Buget <jbuget@gmail.com>
2017-06-23 20:26:37 +0200

    Remove unused dependencies

--- client/test/unit/specs/JobList.spec.js
+++ client/test/unit/specs/JobList.spec.js
@@ -55,54 +55,38 @@ describe('JobList.vue', () => {
 
 	});
 
-	it('should render as many jobs as received from the API', () => {
+	it('should render as many jobs as received from the API', () => Vue.nextTick().then(() => {
 
-		return Vue.nextTick().then(() => {
+		const jobCards = component.$el.querySelectorAll('.job-card');
+		expect(jobCards.length).to.equal(1);
 
-			const jobCards = component.$el.querySelectorAll('.job-card');
-			expect(jobCards.length).to.equal(1);
+	}));
 
-		});
+	it('should render the details of a job', () => Vue.nextTick().then(() => {
 
-	});
+		const firstJobCard = component.$el.querySelectorAll('.job-card')[0];
+		expect(firstJobCard.querySelector('.job__title').textContent).to.equal('Tech Lead');
+		expect(firstJobCard.querySelector('.job__mission').textContent).to.equal('Delivery PUBLICIS / TITAN');
+		expect(firstJobCard.querySelector('.job__client').textContent).to.equal('La Poste - Courrier');
+		expect(firstJobCard.querySelector('.job__start-date').textContent).to.equal('juillet 2017');
+		expect(firstJobCard.querySelector('.job__duration').textContent).to.equal('10 mois');
+		expect(firstJobCard.querySelector('.job__location').textContent).to.equal('OCTO');
+		expect(firstJobCard.querySelector('.job__content').getAttribute('href')).to.equal('https://octopod.octo.com/projects/123456');
 
-	it('should render the details of a job', () => {
+	}));
 
-		return Vue.nextTick().then(() => {
+	it('should render the appropriate status class', () => Vue.nextTick().then(() => {
 
-			const firstJobCard = component.$el.querySelectorAll('.job-card')[0];
-			expect(firstJobCard.querySelector('.job__title').textContent).to.equal('Tech Lead');
-			expect(firstJobCard.querySelector('.job__mission').textContent).to.equal('Delivery PUBLICIS / TITAN');
-			expect(firstJobCard.querySelector('.job__client').textContent).to.equal('La Poste - Courrier');
-			expect(firstJobCard.querySelector('.job__start-date').textContent).to.equal('juillet 2017');
-			expect(firstJobCard.querySelector('.job__duration').textContent).to.equal('10 mois');
-			expect(firstJobCard.querySelector('.job__location').textContent).to.equal('OCTO');
-			expect(firstJobCard.querySelector('.job__content').getAttribute('href')).to.equal('https://octopod.octo.com/projects/123456');
+		const firstJobCard = component.$el.querySelectorAll('.job-card')[0];
+		expect(firstJobCard.querySelector('.job__status').getAttribute('class')).to.contain('job__status--proposal-in-progress');
 
-		});
+	}));
 
-	});
+	it('should add number of available jobs', () => Vue.nextTick().then(() => {
 
-	it('should render the appropriate status class', () => {
+		expect(component.$el.querySelector('.job-results__title').textContent.trim()).to.equal('Missions (1)');
 
-		return Vue.nextTick().then(() => {
-
-			const firstJobCard = component.$el.querySelectorAll('.job-card')[0];
-			expect(firstJobCard.querySelector('.job__status').getAttribute('class')).to.contain('job__status--proposal-in-progress');
-
-		});
-
-	});
-
-	it('should add number of available jobs', () => {
-
-		return Vue.nextTick().then(() => {
-
-			expect(component.$el.querySelector('.job-results__title').textContent.trim()).to.equal('Missions (1)');
-
-		});
-
-	});
+	}));
 
 });
 
