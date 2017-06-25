61f326fd8293b4ede7a72354cf70d75b79efca54
Jérémy Buget <jbuget@octo.com>
2017-06-25 10:11:16 +0200

    Add navigation guard for client route index

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import JobList from '@/components/JobList';
 import LoginPage from '@/components/LoginPage';
+import authenticationService from '@/services/authentication';
 
 Vue.use(Router);
 
@@ -11,6 +12,14 @@ export default new Router({
 			path: '/',
 			name: 'JobList',
 			component: JobList,
+      beforeEnter(to, from, next) {
+        if (! authenticationService.isAuthenticated()) {
+          next('/login');
+        } else {
+          next()
+        }
+
+      }
 		},
 		{
 			path: '/login',
