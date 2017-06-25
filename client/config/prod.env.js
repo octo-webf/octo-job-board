1786c2813696602ce2f46e6bc13d1dae149bc2ce
Jérémy Buget <jbuget@octo.com>
2017-06-25 09:57:24 +0200

    Replace OCTO Google client ID by JBU's one (in order to fix the app for tuesday)

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"/"',
-  CLIENT_ID: '"1095728091059-u6m5nusuq5mdqjb0ddlufgrd6fo7gn06.apps.googleusercontent.com"',
+  CLIENT_ID: '"1095728091059-hke1g2idu0vh3knb4uuk4872jb18d8vu.apps.googleusercontent.com"',
   ANALYTICS_ID: '"UA-3350117-44"',
 }
