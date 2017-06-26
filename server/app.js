d2f5aedf73d0280b6e7674409962532d5aca77bd
Pierre Trollé <ptrolle@octo.com>
2017-06-26 19:52:02 +0200

    add resource to favicon

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -13,6 +14,7 @@ const auth = require('./src/routes/auth');
 const app = express();
 
 // uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
