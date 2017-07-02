a3ab540a3664574fb050cf1ebca156096594e016
Jérémy Buget <jbuget@gmail.com>
2017-07-02 13:19:20 +0200

    Fix linter

--- client/src/components/GithubCorner.vue
+++ client/src/components/GithubCorner.vue
@@ -11,12 +11,12 @@
 </template>
 <script>
   export default {
-    data() {
-      return {
-      	repositoryUrl: 'https://github.com/octo-web-front-end-tribe/octo-job-board'
-      }
-    }
-  }
+  	data() {
+  		return {
+			repositoryUrl: 'https://github.com/octo-web-front-end-tribe/octo-job-board',
+  		};
+  	},
+  };
 </script>
 <style>
   .github-corner__link:hover .octo-arm {
--- client/test/unit/specs/components/GithubCorner.spec.js
+++ client/test/unit/specs/components/GithubCorner.spec.js
@@ -2,30 +2,29 @@ import Vue from 'vue';
 import GithubCorner from '@/components/GithubCorner';
 
 describe('Unit | Component | GithubCorner.vue', () => {
+	let component;
 
-  let component;
-
-  beforeEach(() => {
+	beforeEach(() => {
     // given
-    const Constructor = Vue.extend(GithubCorner);
+		const Constructor = Vue.extend(GithubCorner);
 
     // when
-    component = new Constructor().$mount();
-  });
+		component = new Constructor().$mount();
+	});
 
-  describe('rendering', () => {
-    it('should display a link to GitHub repository', () => {
-      expect(component.$el.querySelector('.github-corner__link')).to.exist;
-    });
+	describe('rendering', () => {
+		it('should display a link to GitHub repository', () => {
+			expect(component.$el.querySelector('.github-corner__link')).to.exist;
+		});
 
-    it('should redirect to "https://github.com/octo-web-front-end-tribe/octo-job-board"', () => {
-      const link = component.$el.querySelector('.github-corner__link');
-      expect(link.getAttribute('href')).to.equal('https://github.com/octo-web-front-end-tribe/octo-job-board');
-    });
+		it('should redirect to "https://github.com/octo-web-front-end-tribe/octo-job-board"', () => {
+			const link = component.$el.querySelector('.github-corner__link');
+			expect(link.getAttribute('href')).to.equal('https://github.com/octo-web-front-end-tribe/octo-job-board');
+		});
 
-    it('should load content in a new tab', () => {
-      const link = component.$el.querySelector('.github-corner__link');
-      expect(link.getAttribute('target')).to.equal('_blank');
-    });
-  });
+		it('should load content in a new tab', () => {
+			const link = component.$el.querySelector('.github-corner__link');
+			expect(link.getAttribute('target')).to.equal('_blank');
+		});
+	});
 });
