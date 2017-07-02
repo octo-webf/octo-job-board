68a5a83efe147bd793c5e04cd1e19cf0e74e5596
Jérémy Buget <jbuget@gmail.com>
2017-07-02 22:57:05 +0200

    Unify server and client ESLint config

--- client/src/api/interests.js
+++ client/src/api/interests.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 const InterestsApi = {
 
-	sendInterest(job, consultant, accessToken) {
-		const url = `${process.env.API_URL}api/interests`;
-		const body = {
-			interestedConsultant: consultant,
-			businessContactNickname: (job.project.business_contact) ? job.project.business_contact.nickname : 'N/A',
-			missionDirectorNickname: (job.project.mission_director) ? job.project.mission_director.nickname : 'N/A',
-			octopodLink: `https://octopod.octo.com/projects/${job.project.id}`,
-			activityName: job.activity.title,
-			missionName: job.project.name,
-		};
-		const options = { headers: { Authorization: `Bearer ${accessToken}` } };
+  sendInterest(job, consultant, accessToken) {
+    const url = `${process.env.API_URL}api/interests`;
+    const body = {
+      interestedConsultant: consultant,
+      businessContactNickname: (job.project.business_contact) ? job.project.business_contact.nickname : 'N/A',
+      missionDirectorNickname: (job.project.mission_director) ? job.project.mission_director.nickname : 'N/A',
+      octopodLink: `https://octopod.octo.com/projects/${job.project.id}`,
+      activityName: job.activity.title,
+      missionName: job.project.name,
+    };
+    const options = { headers: { Authorization: `Bearer ${accessToken}` } };
 
-		return axios.post(url, body, options)
+    return axios.post(url, body, options)
       .then(response => Promise.resolve(response.data))
       .catch(error => Promise.reject(error));
-	},
+  },
 };
 
 export default InterestsApi;
--- client/test/unit/specs/api/jobs.spec.js
+++ client/test/unit/specs/api/jobs.spec.js
@@ -2,50 +2,50 @@ import axios from 'axios';
 import api from '@/api/jobs';
 
 describe('Unit | API | jobs api', () => {
-	describe('#fetchAll', () => {
-		beforeEach(() => {
-			const stubbedResponse = {
-				status: 200,
-				data: {
-					foo: 'bar',
-				},
-			};
-			sinon.stub(axios, 'get').resolves(stubbedResponse);
-		});
+  describe('#fetchAll', () => {
+    beforeEach(() => {
+      const stubbedResponse = {
+        status: 200,
+        data: {
+          foo: 'bar',
+        },
+      };
+      sinon.stub(axios, 'get').resolves(stubbedResponse);
+    });
 
-		afterEach(() => {
-			axios.get.restore();
-		});
+    afterEach(() => {
+      axios.get.restore();
+    });
 
-		it('should fetch API with the good params', () => {
+    it('should fetch API with the good params', () => {
       // given
-			const accessToken = 'valid-access-token';
+      const accessToken = 'valid-access-token';
 
-			const expectedUrl = 'http://localhost:3000/api/jobs';
-			const expectedOptions = { headers: { Authorization: `Bearer ${accessToken}` } };
+      const expectedUrl = 'http://localhost:3000/api/jobs';
+      const expectedOptions = { headers: { Authorization: `Bearer ${accessToken}` } };
 
       // when
-			const promise = api.fetchAll(accessToken);
+      const promise = api.fetchAll(accessToken);
 
       // then
-			return promise.then(() => {
-				expect(axios.get).to.have.been.calledWith(expectedUrl, expectedOptions);
-			});
-		});
+      return promise.then(() => {
+        expect(axios.get).to.have.been.calledWith(expectedUrl, expectedOptions);
+      });
+    });
 
-		it('should return a rejected promise when an error is thrown', (done) => {
+    it('should return a rejected promise when an error is thrown', (done) => {
       // given
-			const accessToken = 'invalid-access_token';
-			axios.get.rejects(new Error('some error'));
+      const accessToken = 'invalid-access_token';
+      axios.get.rejects(new Error('some error'));
 
       // when
-			const promise = api.fetchAll(accessToken);
+      const promise = api.fetchAll(accessToken);
 
       // then
-			promise.catch((error) => {
-				expect(error.message).to.equal('some error');
-				done();
-			});
-		});
-	});
+      promise.catch((error) => {
+        expect(error.message).to.equal('some error');
+        done();
+      });
+    });
+  });
 });
