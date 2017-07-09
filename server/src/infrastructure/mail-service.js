6c088d092729430a7d74ed110f712bdf9e7964c9
Jérémy Buget <jbuget@gmail.com>
2017-07-09 23:28:59 +0200

    Fix linting errors after rebase

--- server/src/infrastructure/mail-service.js
+++ server/src/infrastructure/mail-service.js
@@ -1,45 +1,45 @@
-const mailJet = require('./mailjet')
-const config = require('../config')
+const mailJet = require('./mailjet');
+const config = require('../config');
 
-function sendInterestEmail (form) {
-  const {interestedConsultant} = form
-  const subject = `[JobBoard] ${interestedConsultant.name} intéressé·e par ${form.missionName} - ${form.activityName}`
+function sendInterestEmail(form) {
+  const { interestedConsultant } = form;
+  const subject = `[JobBoard] ${interestedConsultant.name} intéressé·e par ${form.missionName} - ${form.activityName}`;
 
   const template = `
     <h3><a href="mailto:${interestedConsultant.email}">${interestedConsultant.name}</a> est intéressé·e par la mission <strong>${form.missionName}</strong> en tant que <strong>${form.activityName}</strong>.</h3>
     <p>Voir la <a href="${form.octopodLink}">page mission</a></p>
     <p>Contacter le Contact commercial : <a href="https://askbob.octo.com/users/${form.businessContactNickname.toLowerCase()}">${form.businessContactNickname}</a></p>
     <p>Contacter le Directeur de mission : <a href="https://askbob.octo.com/users/${form.missionDirectorNickname.toLowerCase()}">${form.missionDirectorNickname}</a></p>
-    `
+    `;
 
   const options = {
     from: config.MAIL_FROM,
     fromName: 'Le Job Board - Ne pas répondre',
     to: config.MAIL_TO,
-    subject: subject,
-    template: template
-  }
+    subject,
+    template,
+  };
 
-  return mailJet.sendEmail(options)
+  return mailJet.sendEmail(options);
 }
 
-function sendFeedbackEmail (form) {
-  const consultant = form.consultant
-  const subject = `[JobBoard] [Support] ${consultant.name} a émis un message`
-  const template = `${form.feedback}`
+function sendFeedbackEmail(form) {
+  const consultant = form.consultant;
+  const subject = `[JobBoard] [Support] ${consultant.name} a émis un message`;
+  const template = `${form.feedback}`;
 
   const options = {
     from: 'jobboard@octo.com',
     fromName: 'Le Job Board - Ne pas répondre',
     to: 'jobboard@octo.com',
-    subject: subject,
-    template: template
-  }
+    subject,
+    template,
+  };
 
-  return mailJet.sendEmail(options)
+  return mailJet.sendEmail(options);
 }
 
 module.exports = {
   sendInterestEmail,
-  sendFeedbackEmail
-}
+  sendFeedbackEmail,
+};
--- server/src/routes/feedbacks.js
+++ server/src/routes/feedbacks.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const auth = require('../infrastructure/middlewares/auth')
-const mailService = require('../infrastructure/mail-service')
-const router = express.Router()
+const express = require('express');
+const auth = require('../infrastructure/middlewares/auth');
+const mailService = require('../infrastructure/mail-service');
+
+const router = express.Router();
 
 router.post('/', auth, (req, res) => {
-  const form = req.body
+  const form = req.body;
   mailService.sendFeedbackEmail(form)
     .then(() => {
-      res.status(201).json('Feedback sent')
+      res.status(201).json('Feedback sent');
     })
-    .catch(err => {
-      res.status(500).json({error: err})
-    })
-})
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
 
-module.exports = router
+module.exports = router;
