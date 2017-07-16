9fcdd1fa6e207bc6a7802b0b42b2ecabcf9cbcae
Pierre Trollé <ptrolle@octo.com>
2017-07-16 18:10:38 +0200

    tech(JOB-17) : tester l'url du certifcat

---
+++ server/test/integration/routes/certificate.spec.js
@@ -0,0 +1,17 @@
+const { request, expect } = require('../../test-helper');
+const app = require('../../../app');
+
+describe('Integration | Routes | certificate route', () => {
+  it('should have api informations on root', (done) => {
+    request(app)
+      .get('/.well-known/acme-challenge/:content')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        expect(res.text).to.equal('Fz4KwoxqNbaEMWxcBM54Z2or-bYQNN_2Ypbv5Xuxpws.4M0QKfiH4yWeNOiLqoHVHpBHwxEpo1yiMyUHTclOD0s');
+        done();
+      });
+  });
+});
