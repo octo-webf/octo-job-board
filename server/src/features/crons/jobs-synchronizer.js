33b59965b4b6d384bd943967ff8dfa21073d4eb2
Jérémy Buget <jbuget@gmail.com>
2017-08-17 00:10:26 +0200

    Refactor the jobs synchronization scheduler to be clearer (extract some logic into domain/services layer in order to make testing better)

--- server/src/features/crons/jobs-synchronizer.js
+++ server/src/features/crons/jobs-synchronizer.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-console*/
 const scheduler = require('node-schedule');
 const jobService = require('../../domain/services/job-service');
 
 const EVERY_15_MN = '*/15 * * * *';
 
 scheduler.scheduleJob(EVERY_15_MN, () => {
-  // eslint-disable-next-line no-console
-  console.log('Synchronize available jobs from Octopod.');
-  jobService.synchronizeJobs();
+  console.log('Synchronize jobs from Octopod...');
+
+  return jobService.synchronizeJobs()
+    .then(() => {
+      console.log('Synchronization successfull.');
+    })
+    .catch(err => {
+      console.error('Synchronization failed');
+      console.error(err);
+    });
 });
 
 // TODO find a way to test this (unit or integration) :-/
