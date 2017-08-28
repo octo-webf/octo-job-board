cdda59a6d28807b5c8d0433f9ee31beb25870971
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2017-08-28 20:24:56 +1000

    Be able to filter jobs by country

---
+++ client/src/components/JobListFilters.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="job-results__filters">
+
+    <div>
+      <input type="radio" id="anyCountry" value="anyCountry" v-model="onlyOneCountry">
+      <label for="anyCountry">
+        En France et au-delà
+      </label>
+    </div>
+    <div>
+      <input type="radio" id="onlyFrance" value="France" v-model="onlyOneCountry">
+      <label for="onlyFrance">
+        Uniquement en France
+      </label>
+    </div>
+
+    <br>
+
+    <div>
+      <input type="radio" id="onlyAustralia" value="Australia" v-model="onlyOneCountry">
+      <label for="onlyAustralia">
+        Uniquement en Australie
+      </label>
+    </div>
+    <div>
+      <input type="radio" id="onlyMorocco" value="Maroc" v-model="onlyOneCountry">
+      <label for="onlyMorocco">
+        Uniquement au Maroc
+      </label>
+    </div>
+    <div>
+      <input type="radio" id="onlySwitzerland" value="Suisse" v-model="onlyOneCountry">
+      <label for="onlySwitzerland">
+        Uniquement en Suisse
+      </label>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+    name: 'JobListFilters',
+
+    data() {
+      return {
+        onlyOneCountry: 'anyCountry',
+      };
+    },
+
+    watch: {
+      onlyOneCountry(newCountryFilter) {
+        this.$emit('selectCountryFilter', newCountryFilter);
+      },
+    },
+
+  };
+</script>
+
+<style scoped>
+  .job-results__filters > div {
+    display: inline-block;
+    margin: 5px;
+  }
+
+  input,
+  label {
+    cursor: pointer;
+  }
+</style>
