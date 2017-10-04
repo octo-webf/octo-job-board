35bce97a85a05dfe33d932f99e7c8ebf78576701
Elsa Touzeau <etouzeau@octo.com>
2017-10-04 17:25:17 +0200

    Corrige l'accès à la date de staffing de l'objet job

--- client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
+++ client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
@@ -1,10 +1,10 @@
 import projectStaffingNeededDate from '@/utils/projectStaffingNeededDate';
 
 describe('Unit | Utils | Project Staffing Needed Date', () => {
-  it('should sort two jobs according to project staffing needed date', () => {
+  it.only('should sort two jobs according to project staffing needed date', () => {
     // Given
-    const job1 = { id: 1, project: { staffing_needed_from: '2017-11-03' } };
-    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-02' } };
+    const job1 = { id: 1, activity: { staffing_needed_from: '2017-11-03' } };
+    const job2 = { id: 2, activity: { staffing_needed_from: '2017-10-02' } };
 
     const givenJobs = [job1, job2];
     const expectedJobs = [job2, job1];
@@ -18,11 +18,11 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
 
   it('should sort five jobs according to project staffing needed date', () => {
     // Given
-    const job1 = { id: 1, project: { staffing_needed_from: '2017-10-04' } };
-    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-03' } };
-    const job3 = { id: 3, project: { staffing_needed_from: '2017-10-02' } };
-    const job4 = { id: 4, project: { staffing_needed_from: '2017-10-01' } };
-    const job5 = { id: 5, project: { staffing_needed_from: '2017-10-05' } };
+    const job1 = { id: 1, activity: { staffing_needed_from: '2017-10-04' } };
+    const job2 = { id: 2, activity: { staffing_needed_from: '2017-10-03' } };
+    const job3 = { id: 3, activity: { staffing_needed_from: '2017-10-02' } };
+    const job4 = { id: 4, activity: { staffing_needed_from: '2017-10-01' } };
+    const job5 = { id: 5, activity: { staffing_needed_from: '2017-10-05' } };
 
     const givenJobs = [job1, job2, job3, job4, job5];
     const expectedJobs = [job1, job5, job2, job3, job4 ];
@@ -37,11 +37,11 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
 
   it('should sort five jobs with the same date according to project staffing needed date', () => {
     // Given
-    const job1 = { id: 1, project: { staffing_needed_from: '2017-10-04' } };
-    const job3 = { id: 3, project: { staffing_needed_from: '2017-10-04' } };
-    const job4 = { id: 4, project: { staffing_needed_from: '2017-10-04' } };
-    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-04' } };
-    const job5 = { id: 5, project: { staffing_needed_from: '2017-10-04' } };
+    const job1 = { id: 1, activity: { staffing_needed_from: '2017-10-04' } };
+    const job3 = { id: 3, activity: { staffing_needed_from: '2017-10-04' } };
+    const job4 = { id: 4, activity: { staffing_needed_from: '2017-10-04' } };
+    const job2 = { id: 2, activity: { staffing_needed_from: '2017-10-04' } };
+    const job5 = { id: 5, activity: { staffing_needed_from: '2017-10-04' } };
 
     const givenJobs = [job1, job2, job3, job4, job5];
     const expectedJobs = [job1, job2, job3, job4, job5];
@@ -54,12 +54,12 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
 
   it('should sort jobs according to project staffing needed date', () => {
     // Given
-    const job1 = { id: 1, project: { staffing_needed_from: '2017-11-03' } };
-    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-02' } };
-    const job3 = { id: 3, project: { staffing_needed_from: '2017-10-04' } };
-    const job4 = { id: 4, project: { staffing_needed_from: '2017-09-15' } };
-    const job5 = { id: 5, project: { staffing_needed_from: '2017-10-03' } };
-    const job6 = { id: 6, project: { staffing_needed_from: '2017-09-08' } };
+    const job1 = { id: 1, activity: { staffing_needed_from: '2017-11-03' } };
+    const job2 = { id: 2, activity: { staffing_needed_from: '2017-10-02' } };
+    const job3 = { id: 3, activity: { staffing_needed_from: '2017-10-04' } };
+    const job4 = { id: 4, activity: { staffing_needed_from: '2017-09-15' } };
+    const job5 = { id: 5, activity: { staffing_needed_from: '2017-10-03' } };
+    const job6 = { id: 6, activity: { staffing_needed_from: '2017-09-08' } };
 
 
     const givenJobs = [job1, job2, job3, job4, job5, job6];
