13a5e734b44efc93caa0f760984f0e0c9929e9c7
Elsa Touzeau <etouzeau@octo.com>
2017-10-04 12:16:27 +0200

    Finalisation de la fonction de tri des dates de besoin de staffing, en fonction du jour daujourdhui

--- client/src/utils/projectStaffingNeededDate.js
+++ client/src/utils/projectStaffingNeededDate.js
@@ -1,49 +1,38 @@
+import moment from 'moment';
 export default {
 
-  _convertStringDateToDate(stringDate) {
-    let dateArray = stringDate.split('-');
-    return new Date( parseInt(dateArray[0]), parseInt(dateArray[1]) - 1 , parseInt(dateArray[2]));
-  },
-
-  _datesAreEqual(date1, date2) {
-    return (date1.getFullYear() === date2.getFullYear()
-      && date1.getMonth() === date2.getMonth()
-      && date1.getDay() === date2.getDay());
+  _toggleSignAndAddOneDay(counter){
+    return counter > 0 ? counter * -1 : counter * -1 + 1 ;
   },
 
   sort(jobs) {
 
-    let jobsCopy  = jobs;
+    let jobsToBeSorted  = jobs;
     let jobsSorted = [];
-    let today = new Date();
+    let today = moment();
+    let counterNDays = 0;
 
-    jobsCopy.forEach((job) => {
-      let jobDate = this._convertStringDateToDate(job.project.staffing_needed_from);
+    while(jobsToBeSorted.length){
 
-      if(this._datesAreEqual(jobDate, today)){
-        jobsSorted.push(job);
-      }
-      else {
-        for(let i=1; i < 3 ; i++) {
-          let todayPlus = new Date( today.getTime() );
-          todayPlus.setDate(todayPlus.getDate() + i);
-          let todayMinus = new Date( today.getTime() );
-          todayMinus.setDate(todayMinus.getDate() - i);
+      let jobsToKeepForNextIteration =[];
+      let jobPlusMinusDay = today.clone().add(counterNDays, 'day');
 
-          console.log('currentJobDate : ' + jobDate );
-          console.log('todayPlus: ' + todayPlus);
-          console.log('todayMinus: ' + todayMinus);
-          console.log('todayPlus compare : ' + this._datesAreEqual(jobDate, todayPlus));
-          console.log('todayMinus compare : ' + this._datesAreEqual(jobDate, todayMinus));
+      jobsToBeSorted.forEach((job) => {
+        let jobDate = moment(job.project.staffing_needed_from, 'YYYY-MM-DD');
+
+        if(jobDate.isSame(jobPlusMinusDay, 'day')){
+          jobsSorted.push(job);
         }
-      }
+        else {
+          jobsToKeepForNextIteration.push(job);
+        }
+      });
 
+      counterNDays = this._toggleSignAndAddOneDay(counterNDays);
+      jobsToBeSorted =jobsToKeepForNextIteration;
+    }
 
-
-    });
-
-    //console.log('jobsSorted : ', jobsSorted);
-    return [ jobs[1], jobs[0] ];
+    return jobsSorted;
   },
 
 };
--- client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
+++ client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
@@ -16,15 +16,16 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
     expect(sortedJobs).to.deep.equal(expectedJobs);
   });
 
-  it('should sort four jobs according to project staffing needed date', () => {
+  it('should sort five jobs according to project staffing needed date', () => {
     // Given
-    const job1 = { id: 1, project: { staffing_needed_from: '2017-11-03' } };
-    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-02' } };
-    const job3 = { id: 3, project: { staffing_needed_from: '2017-09-30' } };
-    const job4 = { id: 4, project: { staffing_needed_from: '2017-10-03' } };
+    const job1 = { id: 1, project: { staffing_needed_from: '2017-10-04' } };
+    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-03' } };
+    const job3 = { id: 3, project: { staffing_needed_from: '2017-10-02' } };
+    const job4 = { id: 4, project: { staffing_needed_from: '2017-10-01' } };
+    const job5 = { id: 5, project: { staffing_needed_from: '2017-10-05' } };
 
-    const givenJobs = [job1, job2, job3, job4];
-    const expectedJobs = [job4, job2, job3, job1 ];
+    const givenJobs = [job1, job2, job3, job4, job5];
+    const expectedJobs = [job1, job5, job2, job3, job4 ];
 
     // When
     const sortedJobs = projectStaffingNeededDate.sort(givenJobs);
@@ -33,7 +34,25 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
     expect(sortedJobs).to.deep.equal(expectedJobs);
   });
 
-  it.skip('should sort jobs according to project staffing needed date', () => {
+
+  it('should sort five jobs with the same date according to project staffing needed date', () => {
+    // Given
+    const job1 = { id: 1, project: { staffing_needed_from: '2017-10-04' } };
+    const job3 = { id: 3, project: { staffing_needed_from: '2017-10-04' } };
+    const job4 = { id: 4, project: { staffing_needed_from: '2017-10-04' } };
+    const job2 = { id: 2, project: { staffing_needed_from: '2017-10-04' } };
+    const job5 = { id: 5, project: { staffing_needed_from: '2017-10-04' } };
+
+    const givenJobs = [job1, job2, job3, job4, job5];
+    const expectedJobs = [job1, job2, job3, job4, job5];
+
+    // When
+    const sortedJobs = projectStaffingNeededDate.sort(givenJobs);
+    // Then
+    expect(sortedJobs).to.deep.equal(expectedJobs);
+  });
+
+  it('should sort jobs according to project staffing needed date', () => {
     // Given
     const job1 = { id: 1, project: { staffing_needed_from: '2017-11-03' } };
     const job2 = { id: 2, project: { staffing_needed_from: '2017-10-02' } };
@@ -44,7 +63,7 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
 
 
     const givenJobs = [job1, job2, job3, job4, job5, job6];
-    const expectedJobs = [job5, job3, job2, job4, job6, job1];
+    const expectedJobs = [job3, job5, job2, job4, job6, job1];
 
     // When
     const sortedJobs = projectStaffingNeededDate.sort(givenJobs);
