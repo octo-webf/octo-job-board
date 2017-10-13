fc55cc76fa38e9381d444363661e9fda1865697c
Issel Parra <iparra@octo.com>
2017-10-13 16:07:56 +0200

    Improves sort by date test

--- client/src/utils/projectStaffingNeededDate.js
+++ client/src/utils/projectStaffingNeededDate.js
@@ -1,35 +1,34 @@
 import moment from 'moment';
 
+
+function _sortByDaysFromToday(jobs) {
+  return jobs.sort((job1, job2) => {
+    const daysFromToday1 = job1.daysFromToday >= 0 ? job1.daysFromToday : (job1.daysFromToday * -1) + 0.1;
+    const daysFromToday2 = job2.daysFromToday >= 0 ? job2.daysFromToday : (job2.daysFromToday * -1) + 0.1;
+
+    if (daysFromToday1 < daysFromToday2) {
+      return -1;
+    }
+    if (daysFromToday1 > daysFromToday2) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+function _setDaysFromToday(today, jobs) {
+  jobs.map((job) => {
+    const jobWithDaysFromToday = job;
+    const jobDate = moment(job.activity.staffing_needed_from, 'YYYY-MM-DD');
+    jobWithDaysFromToday.daysFromToday = jobDate.diff(today, 'days');
+    return jobWithDaysFromToday;
+  });
+}
+
 export default {
-
-  _sortByDaysFromToday(jobs) {
-    return jobs.sort((job1, job2) => {
-      const daysFromToday1 = job1.daysFromToday >= 0 ? job1.daysFromToday : (job1.daysFromToday * -1) + 0.1;
-      const daysFromToday2 = job2.daysFromToday >= 0 ? job2.daysFromToday : (job2.daysFromToday * -1) + 0.1;
-
-      if (daysFromToday1 < daysFromToday2) {
-        return -1;
-      }
-      if (daysFromToday1 > daysFromToday2) {
-        return 1;
-      }
-      return 0;
-    });
-  },
-
-  _setDaysFromToday(today, jobs) {
-    jobs.map((job) => {
-      const jobWithDaysFromToday = job;
-      const jobDate = moment(job.activity.staffing_needed_from, 'YYYY-MM-DD');
-      jobWithDaysFromToday.daysFromToday = jobDate.diff(today, 'days');
-      return jobWithDaysFromToday;
-    });
-  },
-
   sort(today, jobs) {
     const jobsToBeSorted = jobs;
-    this._setDaysFromToday(today, jobsToBeSorted);
-    return this._sortByDaysFromToday(jobsToBeSorted);
+    _setDaysFromToday(today, jobsToBeSorted);
+    return _sortByDaysFromToday(jobsToBeSorted);
   },
-
 };
