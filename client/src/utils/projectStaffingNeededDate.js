860f8033da78f8a25fb29f89cad376473112e10f
Elsa Touzeau <etouzeau@octo.com>
2017-10-16 10:40:31 +0200

    fix(JOB-135) : reverts projectStaffingNeededDate.js

--- client/src/utils/projectStaffingNeededDate.js
+++ client/src/utils/projectStaffingNeededDate.js
@@ -1,34 +1,35 @@
 import moment from 'moment';
 
-function _sortByDaysFromToday(jobs) {
-  return jobs.sort((job1, job2) => {
-
-    if (Math.abs(job1.daysFromToday) < Math.abs(job2.daysFromToday)) {
-      return -1;
-    }
-    if (Math.abs(job1.daysFromToday) > Math.abs(job2.daysFromToday)) {
-      return 1;
-    }
-    if (job1.daysFromToday < job2.daysFromToday) {
-      return 1;
-    }
-    return 0;
-  });
-}
-
-function _setDaysFromToday(today, jobs) {
-  jobs.map((job) => {
-    const jobWithDaysFromToday = job;
-    const jobDate = moment(job.activity.staffing_needed_from, 'YYYY-MM-DD');
-    jobWithDaysFromToday.daysFromToday = jobDate.diff(today, 'days');
-    return jobWithDaysFromToday;
-  });
-}
-
 export default {
+
+  _sortByDaysFromToday(jobs) {
+    return jobs.sort((job1, job2) => {
+      const daysFromToday1 = job1.daysFromToday >= 0 ? job1.daysFromToday : (job1.daysFromToday * -1) + 0.1;
+      const daysFromToday2 = job2.daysFromToday >= 0 ? job2.daysFromToday : (job2.daysFromToday * -1) + 0.1;
+
+      if (daysFromToday1 < daysFromToday2) {
+        return -1;
+      }
+      if (daysFromToday1 > daysFromToday2) {
+        return 1;
+      }
+      return 0;
+    });
+  },
+
+  _setDaysFromToday(today, jobs) {
+    jobs.map((job) => {
+      const jobWithDaysFromToday = job;
+      const jobDate = moment(job.activity.staffing_needed_from, 'YYYY-MM-DD');
+      jobWithDaysFromToday.daysFromToday = jobDate.diff(today, 'days');
+      return jobWithDaysFromToday;
+    });
+  },
+
   sort(today, jobs) {
     const jobsToBeSorted = jobs;
-    _setDaysFromToday(today, jobsToBeSorted);
-    return _sortByDaysFromToday(jobsToBeSorted);
+    this._setDaysFromToday(today, jobsToBeSorted);
+    return this._sortByDaysFromToday(jobsToBeSorted);
   },
+
 };
