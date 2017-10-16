31f5016913f6f1081986631e406e23adadff67d1
Elsa Touzeau <etouzeau@octo.com>
2017-10-16 09:59:05 +0200

    fix(JOB-135) : remplacement des ternaires par l'utilisation des valeurs absolues

--- client/src/utils/projectStaffingNeededDate.js
+++ client/src/utils/projectStaffingNeededDate.js
@@ -3,13 +3,14 @@ import moment from 'moment';
 
 function _sortByDaysFromToday(jobs) {
   return jobs.sort((job1, job2) => {
-    const daysFromToday1 = job1.daysFromToday >= 0 ? job1.daysFromToday : (job1.daysFromToday * -1) + 0.1;
-    const daysFromToday2 = job2.daysFromToday >= 0 ? job2.daysFromToday : (job2.daysFromToday * -1) + 0.1;
 
-    if (daysFromToday1 < daysFromToday2) {
+    if (Math.abs(job1.daysFromToday) < Math.abs(job2.daysFromToday)) {
       return -1;
     }
-    if (daysFromToday1 > daysFromToday2) {
+    if (Math.abs(job1.daysFromToday) > Math.abs(job2.daysFromToday)) {
+      return 1;
+    }
+    if (job1.daysFromToday < job2.daysFromToday) {
       return 1;
     }
     return 0;
--- client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
+++ client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
@@ -2,15 +2,13 @@ import moment from 'moment';
 import projectStaffingNeededDate from '@/utils/projectStaffingNeededDate';
 
 describe.only('Unit | Utils | Project Staffing Needed Date', () => {
-
-  describe('#sort', () => {
-
+  describe.only('#sort', () => {
     it('should sort two jobs according to project staffing needed date', () => {
       // Given
       const today = moment('2017-10-04', 'YYYY-MM-DD');
 
-      const futureJob = {id: 1, activity: {staffing_needed_from: '2017-11-03'}};
-      const beforeYesterdayJob = {id: 2, activity: {staffing_needed_from: '2017-10-02'}};
+      const futureJob = { id: 1, activity: { staffing_needed_from: '2017-11-03' } };
+      const beforeYesterdayJob = { id: 2, activity: { staffing_needed_from: '2017-10-02' } };
 
       const givenJobs = [futureJob, beforeYesterdayJob];
       const expectedSortedJobs = [beforeYesterdayJob, futureJob];
@@ -26,11 +24,11 @@ describe.only('Unit | Utils | Project Staffing Needed Date', () => {
       // Given
       const today = moment('2017-10-04', 'YYYY-MM-DD');
 
-      const todayJob = {id: 1, activity: {staffing_needed_from: '2017-10-04'}};
-      const yesterdayJob = {id: 2, activity: {staffing_needed_from: '2017-10-03'}};
-      const beforeYesterdayJob = {id: 3, activity: {staffing_needed_from: '2017-10-02'}};
-      const oldJob = {id: 4, activity: {staffing_needed_from: '2017-10-01'}};
-      const tomorrowJob = {id: 5, activity: {staffing_needed_from: '2017-10-05'}};
+      const todayJob = { id: 1, activity: { staffing_needed_from: '2017-10-04' } };
+      const yesterdayJob = { id: 2, activity: { staffing_needed_from: '2017-10-03' } };
+      const beforeYesterdayJob = { id: 3, activity: { staffing_needed_from: '2017-10-02' } };
+      const oldJob = { id: 4, activity: { staffing_needed_from: '2017-10-01' } };
+      const tomorrowJob = { id: 5, activity: { staffing_needed_from: '2017-10-05' } };
 
       const givenJobs = [todayJob, yesterdayJob, beforeYesterdayJob, oldJob, tomorrowJob];
       const expectedSortedJobs = [todayJob, tomorrowJob, yesterdayJob, beforeYesterdayJob, oldJob];
@@ -46,11 +44,11 @@ describe.only('Unit | Utils | Project Staffing Needed Date', () => {
       // Given
       const today = moment('2017-10-04', 'YYYY-MM-DD');
 
-      const todayJob1 = {id: 1, activity: {staffing_needed_from: '2017-10-04'}};
-      const todayJob2 = {id: 2, activity: {staffing_needed_from: '2017-10-04'}};
-      const todayJob3 = {id: 3, activity: {staffing_needed_from: '2017-10-04'}};
-      const todayJob4 = {id: 4, activity: {staffing_needed_from: '2017-10-04'}};
-      const todayJob5 = {id: 5, activity: {staffing_needed_from: '2017-10-04'}};
+      const todayJob1 = { id: 1, activity: { staffing_needed_from: '2017-10-04' } };
+      const todayJob2 = { id: 2, activity: { staffing_needed_from: '2017-10-04' } };
+      const todayJob3 = { id: 3, activity: { staffing_needed_from: '2017-10-04' } };
+      const todayJob4 = { id: 4, activity: { staffing_needed_from: '2017-10-04' } };
+      const todayJob5 = { id: 5, activity: { staffing_needed_from: '2017-10-04' } };
 
       const givenJobs = [todayJob1, todayJob2, todayJob3, todayJob4, todayJob5];
       const expectedSortedJobs = [todayJob1, todayJob2, todayJob3, todayJob4, todayJob5];
@@ -65,12 +63,12 @@ describe.only('Unit | Utils | Project Staffing Needed Date', () => {
       // Given
       const today = moment('2017-10-04', 'YYYY-MM-DD');
 
-      const futureJob = {id: 1, activity: {staffing_needed_from: '2017-11-03'}};
-      const beforeYesterdayJob = {id: 2, activity: {staffing_needed_from: '2017-10-02'}};
-      const todayJob = {id: 3, activity: {staffing_needed_from: '2017-10-04'}};
-      const veryOldJob = {id: 4, activity: {staffing_needed_from: '2017-09-15'}};
-      const yesterdayJob = {id: 5, activity: {staffing_needed_from: '2017-10-03'}};
-      const oldJob = {id: 6, activity: {staffing_needed_from: '2017-09-08'}};
+      const futureJob = { id: 1, activity: { staffing_needed_from: '2017-11-03' } };
+      const beforeYesterdayJob = { id: 2, activity: { staffing_needed_from: '2017-10-02' } };
+      const todayJob = { id: 3, activity: { staffing_needed_from: '2017-10-04' } };
+      const veryOldJob = { id: 4, activity: { staffing_needed_from: '2017-09-15' } };
+      const yesterdayJob = { id: 5, activity: { staffing_needed_from: '2017-10-03' } };
+      const oldJob = { id: 6, activity: { staffing_needed_from: '2017-09-08' } };
 
 
       const givenJobs = [futureJob, beforeYesterdayJob, todayJob, veryOldJob, yesterdayJob, oldJob];
