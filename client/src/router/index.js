e9877dd2c1f371870c73da67b4a73f073ab8265e
Pierre Trollé <ptrolle@octo.com>
2017-11-03 12:04:41 +0100

    feature(JOB-85) : add unsubscription page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import JobList from '@/components/JobList';
 import LoginPage from '@/components/LoginPage';
+import UnsubscribePage from '@/components/UnsubscribePage';
 import authenticationService from '@/services/authentication';
 
 Vue.use(Router);
@@ -24,6 +25,18 @@ export default new Router({
       path: '/login',
       name: 'LoginPage',
       component: LoginPage,
+    },
+    {
+      path: '/unsubscribe',
+      name: 'UnsubscribePage',
+      component: UnsubscribePage,
+      beforeEnter(to, from, next) {
+        if (!authenticationService.isAuthenticated()) {
+          next('/login');
+        } else {
+          next();
+        }
+      },
     },
   ],
 });
