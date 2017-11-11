907ecaa3bf599acd639c760099bd317f54e3ce4a
Clément Dubois <cdubois@octo.com>
2017-11-11 09:34:07 +0100

    Refacto : simplify jobs serializer

--- server/src/infrastructure/serializers/jobs.js
+++ server/src/infrastructure/serializers/jobs.js
@@ -1,20 +1,16 @@
 const JobsSerializer = {
 
   serialize(projects, activities) {
-    const serializedJobs = [];
-
-    activities.reduce((jobs, octopodActivity) => {
-      const activity = {
-        id: octopodActivity.id,
-        title: octopodActivity.title,
-        staffing_needed_from: octopodActivity.staffing_needed_from,
-      };
-      const project = projects.find(p => p.id === octopodActivity.project.id);
-      jobs.push({ project, activity });
-      return jobs;
-    }, serializedJobs);
-
-    return serializedJobs;
+    return activities.map((octopodActivity) => {
+      return {
+        activity : {
+          id: octopodActivity.id,
+          title: octopodActivity.title,
+          staffing_needed_from: octopodActivity.staffing_needed_from,
+        },
+        project : projects.find(p => p.id === octopodActivity.project.id)
+      }
+    });
   },
 };
 
