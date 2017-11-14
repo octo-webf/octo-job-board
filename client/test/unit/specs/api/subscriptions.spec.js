952613cc6c4ced5eb52ddbc2ed028c30e1e7a20b
Pierre Trollé <ptrolle@octo.com>
2017-11-14 11:13:42 +0100

    review(JOB-85) : improve tests by review by VEN

--- client/test/unit/specs/api/subscriptions.spec.js
+++ client/test/unit/specs/api/subscriptions.spec.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 import subscriptionsApi from '@/api/subscriptions';
 
 describe('Unit | API | subscriptions api', () => {
+
+  const accessToken = 'valid-access-token';
+  const expectedUrl = 'http://localhost:3000/api/subscriptions';
+  const expectedOptions = { headers: { Authorization: `Bearer ${accessToken}` } };
+
   describe('#subscription', () => {
+    let stub;
+
     beforeEach(() => {
-      sinon.stub(axios, 'post').resolves();
+      stub = sinon.stub(axios, 'post');
     });
 
     afterEach(() => {
@@ -13,10 +20,7 @@ describe('Unit | API | subscriptions api', () => {
 
     it('should call POST /api/subscriptions', () => {
       // given
-      const accessToken = 'valid-access-token';
-
-      const expectedUrl = 'http://localhost:3000/api/subscriptions';
-      const expectedOptions = { headers: { Authorization: `Bearer ${accessToken}` } };
+      stub.resolves();
 
       // when
       const promise = subscriptionsApi.subscribe(accessToken);
@@ -26,11 +30,39 @@ describe('Unit | API | subscriptions api', () => {
         expect(axios.post).to.have.been.calledWith(expectedUrl, {}, expectedOptions);
       });
     });
+
+    it('should return axios answer', () => {
+      // given
+      stub.resolves('answer');
+
+      // when
+      const promise = subscriptionsApi.subscribe(accessToken);
+
+      // then
+      return promise.then((answer) => {
+        expect(answer).to.equal('answer');
+      });
+    });
+
+    it('should throw error', () => {
+      // given
+      stub.rejects(new Error('error'));
+
+      // when
+      const promise = subscriptionsApi.subscribe(accessToken);
+
+      // then
+      return promise.catch((err) => {
+        expect(err.message).to.equal('error')
+      });
+    });
   });
 
   describe('#unsubscribe', () => {
+    let stub;
+
     beforeEach(() => {
-      sinon.stub(axios, 'delete').resolves();
+      stub = sinon.stub(axios, 'delete');
     });
 
     afterEach(() => {
@@ -39,10 +71,7 @@ describe('Unit | API | subscriptions api', () => {
 
     it('should call DELETE /api/subscriptions', () => {
       // given
-      const accessToken = 'valid-access-token';
-
-      const expectedUrl = 'http://localhost:3000/api/subscriptions';
-      const expectedOptions = { headers: { Authorization: `Bearer ${accessToken}` } };
+      stub.resolves();
 
       // when
       const promise = subscriptionsApi.unsubscribe(accessToken);
@@ -50,6 +79,32 @@ describe('Unit | API | subscriptions api', () => {
       // then
       return promise.then(() => {
         expect(axios.delete).to.have.been.calledWith(expectedUrl, expectedOptions);
+      });
+    });
+
+    it('should return axios answer', () => {
+      // given
+      stub.resolves('answer');
+
+      // when
+      const promise = subscriptionsApi.unsubscribe(accessToken);
+
+      // then
+      return promise.then((answer) => {
+        expect(answer).to.equal('answer');
+      });
+    });
+
+    it('should throw error', () => {
+      // given
+      stub.rejects(new Error('error'));
+
+      // when
+      const promise = subscriptionsApi.unsubscribe(accessToken);
+
+      // then
+      return promise.catch((err) => {
+        expect(err.message).to.equal('error')
       });
     });
   });
