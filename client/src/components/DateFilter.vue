1b8992ba7b5c3438a2021c6d6a74582262185c13
Pierre Trollé <ptrolle@octo.com>
2017-11-20 13:16:20 +0100

    feature(JOB-172) : add datepicker

---
+++ client/src/components/DateFilter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="date date-filter">
+    <datepicker
+        :format="'d MMMM yyyy'"
+        :full-month-name="true"
+        language="fr"
+        class="date-picker"
+        input-class="date-picker__input"
+        :monday-first="true"
+        v-model="date"
+    ></datepicker>
+  </div>
+</template>
+
+<script>
+  import Datepicker from 'vuejs-datepicker';
+
+  export default {
+    name: 'DateFilters',
+    components: { Datepicker },
+    data() {
+      return {
+        date: new Date(),
+      };
+    },
+
+    watch: {
+      date(newChosenDate) {
+        this.$emit('date', newChosenDate);
+      },
+    },
+
+  };
+</script>
+
+<style>
+  .date-picker__input {
+    padding: 9px 20px 9px 10px;
+    font-family: Roboto;
+    /*height: 40px;*/
+    /*width: 250px;*/
+    font-size: 1em;
+    background-color: #FFF;
+  }
+</style>
