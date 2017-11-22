bb9b727a610b7603e7cd70f33c4d54f3634988fc
Pierre Trollé <ptrolle@octo.com>
2017-11-22 19:15:06 +0100

    review(JOB-172) : some bsr by VEN

--- client/src/utils/jobsSorter.js
+++ client/src/utils/jobsSorter.js
@@ -2,19 +2,18 @@ import projectStaffingNeededDate from '@/utils/projectStaffingNeededDate';
 import moment from 'moment';
 
 export default {
-
   sort(jobs, availabityDate = moment()) {
     const regexToFindOnlyMissions = /^mission_/;
-    let jobsWithStatusMission = jobs.filter(job => regexToFindOnlyMissions.test(job.project.status));
-    let jobsWithOtherStatus = jobs.filter(job => !regexToFindOnlyMissions.test(job.project.status));
+    const jobsWithStatusMission = jobs.filter(job => regexToFindOnlyMissions.test(job.project.status));
+    const jobsWithOtherStatus = jobs.filter(job => !regexToFindOnlyMissions.test(job.project.status));
 
-    if (jobsWithStatusMission.length > 1) {
-      jobsWithStatusMission = projectStaffingNeededDate.sortAll(jobsWithStatusMission);
-    }
-    if (jobsWithOtherStatus.length > 1) {
-      jobsWithOtherStatus = projectStaffingNeededDate.sortAfter(jobsWithOtherStatus, availabityDate);
-    }
-    return [...jobsWithStatusMission, ...jobsWithOtherStatus];
+    const sortedJobsWithStatusMission = jobsWithStatusMission.length > 1 ?
+      projectStaffingNeededDate.sortAll(jobsWithStatusMission) :
+      jobsWithStatusMission;
+    const sortedJobsWithOtherStatus = jobsWithOtherStatus.length > 1 ?
+      projectStaffingNeededDate.sortAfter(jobsWithOtherStatus, availabityDate) :
+      jobsWithOtherStatus;
+
+    return [...sortedJobsWithStatusMission, ...sortedJobsWithOtherStatus];
   },
-
 };
--- client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
+++ client/test/unit/specs/utils/projectStaffingNeededDate.spec.js
@@ -76,53 +76,18 @@ describe('Unit | Utils | Project Staffing Needed Date', () => {
       clock.restore();
     });
 
-    it('should sort chronologically two jobs according to project staffing needed date', () => {
+    it('should sort three jobs whose staffing_needed_from chronologically after availability date', () => {
       // Given
       const futureJob = { activity: { id: 1, staffing_needed_from: '2017-11-03' } };
-      const todayJob = { activity: { id: 2, staffing_needed_from: '2017-10-04' } };
+      const todayJob = { activity: { id: 1, staffing_needed_from: '2017-10-04' } };
+      const notAvailableJob = { activity: { id: 3, staffing_needed_from: '2017-09-02' } };
 
-      const givenJobs = [futureJob, todayJob];
+      const givenJobs = [futureJob, todayJob, notAvailableJob];
       const expectedSortedJobs = [todayJob, futureJob];
 
       // When
       const sortedJobs = projectStaffingNeededDate.sortAfter(givenJobs, availabityDate);
 
-      // Then
-      expect(sortedJobs).to.deep.equal(expectedSortedJobs);
-    });
-
-    it('should sort chronologically five jobs according to project staffing needed date', () => {
-      // Given
-      const futureJob = { activity: { id: 1, staffing_needed_from: '2017-11-03' } };
-      const todayJob = { activity: { id: 1, staffing_needed_from: '2017-10-04' } };
-      const yesterdayJob = { activity: { id: 2, staffing_needed_from: '2017-10-03' } };
-      const beforeYesterdayJob = { activity: { id: 3, staffing_needed_from: '2017-10-02' } };
-      const oldJob = { activity: { id: 4, staffing_needed_from: '2017-06-01' } };
-      const tomorrowJob = { activity: { id: 5, staffing_needed_from: '2017-10-05' } };
-
-      const givenJobs = [futureJob, todayJob, yesterdayJob, beforeYesterdayJob, oldJob, tomorrowJob];
-      const expectedSortedJobs = [yesterdayJob, todayJob, tomorrowJob, futureJob];
-
-      // When
-      const sortedJobs = projectStaffingNeededDate.sortAfter(givenJobs, availabityDate);
-
-      // Then
-      expect(sortedJobs).to.deep.equal(expectedSortedJobs);
-    });
-
-    it('should sort chronologically five jobs with the same date according to project staffing needed date', () => {
-      // Given
-      const todayJob1 = { activity: { id: 1, staffing_needed_from: '2017-10-04' } };
-      const todayJob2 = { activity: { id: 2, staffing_needed_from: '2017-10-04' } };
-      const todayJob3 = { activity: { id: 3, staffing_needed_from: '2017-10-04' } };
-      const todayJob4 = { activity: { id: 4, staffing_needed_from: '2017-10-04' } };
-      const todayJob5 = { activity: { id: 5, staffing_needed_from: '2017-10-04' } };
-
-      const givenJobs = [todayJob1, todayJob2, todayJob3, todayJob4, todayJob5];
-      const expectedSortedJobs = [todayJob1, todayJob2, todayJob3, todayJob4, todayJob5];
-
-      // When
-      const sortedJobs = projectStaffingNeededDate.sortAfter(givenJobs, availabityDate);
       // Then
       expect(sortedJobs).to.deep.equal(expectedSortedJobs);
     });
