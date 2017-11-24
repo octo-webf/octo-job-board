f7addccc1bd313bb7bc917e9eb3a72bd6d78ab16
Pierre Trollé <ptrolle@octo.com>
2017-11-24 12:04:45 +0100

    feature(JOB-158) : extract Job Header

--- client/src/components/JobList.vue
+++ client/src/components/JobList.vue
@@ -9,25 +9,10 @@
         <template v-else>
           <div class="job-results-panel">
             <section class="job-results job-results--delivery">
-              <div class="job-results__top">
-                <div class="job-results__filters-left">
-                  <div class="job-filters-left__wrapper filters_wrapper">
-                    <span class="job-filters-left__text">Disponible à partir du </span>
-                    <date-picker @selected="onSelectedAvailabilityDate"></date-picker>
-                  </div>
-                </div>
-                <div class="job-results__title-container">
-                  <h1 class="job-results__title">
-                    Missions à staffer ({{ displayedJobs.length }})
-                  </h1>
-                </div>
-                <div class="job-results__filters-right">
-                  <div class="job-filters-right__wrapper filters_wrapper">
-                    <span class="job-filters-right__text">Provenance des missions</span>
-                    <country-picker @selected="onSelectedCountry"></country-picker>
-                  </div>
-                </div>
-              </div>
+              <job-header :jobsNumber="displayedJobs.length"
+                          @selectedCountry="onSelectedCountry"
+                          @selectedDate="onSelectedAvailabilityDate">
+              </job-header>
               <ul class="job-results__list">
                 <li class="job-results__item" v-for="job in displayedJobs">
                   <job-card v-on:interest="displayInterestModal" :job="job"></job-card>
@@ -48,8 +33,7 @@
   import jobsSorter from '@/utils/jobsSorter';
   import jobsApi from '@/api/jobs';
   import AppHeader from '@/components/AppHeader';
-  import CountryPicker from '@/components/CountryPicker';
-  import DatePicker from '@/components/DatePicker';
+  import JobHeader from '@/components/JobHeader';
   import JobCard from '@/components/JobCard';
   import Circle from 'vue-loading-spinner/src/components/Circle';
   import InterestModal from '@/components/InterestModal';
@@ -61,9 +45,8 @@
 
     components: {
       AppHeader,
-      CountryPicker,
-      DatePicker,
       JobCard,
+      JobHeader,
       InterestModal,
       'circle-loader': Circle,
     },
@@ -135,30 +118,6 @@
     margin-bottom: 60px;
   }
 
-  .job-results__title {
-    font-weight: 300;
-    font-size: 24px;
-    margin: 0 0 15px;
-  }
-
-  .job-results__filters-left, .job-results__filters-right {
-    display: flex;
-    justify-content: center;
-  }
-
-  .filters_wrapper {
-    display: block;
-    text-align: left;
-  }
-
-  .job-filters-left__text {
-    padding-left: 10px;
-  }
-
-  .job-filters-right__text {
-    padding-left: 15px;
-  }
-
   .job-results__list {
     padding: 0;
     display: flex;
@@ -173,30 +132,4 @@
     margin: 10px;
   }
 
-  @media only screen and (min-width: 1240px) {
-    .job-results__top {
-      min-width: 1240px;
-    }
-  }
-
-  @media only screen and (min-width: 992px) {
-    .job-results__top {
-      min-width: 920px;
-    }
-  }
-
-  @media only screen and (min-width: 640px) {
-    .job-results__top {
-      display: flex;
-      min-width: 640px;
-    }
-
-    .job-results__filters-left, .job-results__filters-right {
-      width: 20%;
-    }
-
-    .job-results__title-container {
-      width: 60%;
-    }
-  }
 </style>
