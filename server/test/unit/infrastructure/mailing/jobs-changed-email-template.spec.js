0dde5a35c306d96672a19b199b505450f7def5af
victorenaud <7222886+victorenaud@users.noreply.github.com>
2017-11-28 15:52:13 +0100

    feat(JOB-170): add context to emails for subscribers

--- server/test/unit/infrastructure/mailing/jobs-changed-email-template.spec.js
+++ server/test/unit/infrastructure/mailing/jobs-changed-email-template.spec.js
@@ -3,12 +3,21 @@ const jobsChangedEmailTemplate = require('../../../../src/infrastructure/mailing
 
 describe('Unit | Infrastructure | Mailing | jobs-changed-email-template', () => {
   describe('#compile', () => {
-    const addedJob1 = { project: { name: 'p1' }, activity: { title: 'a1' } };
-    const addedJob2 = { project: { name: 'p2' }, activity: { title: 'a2' } };
-    const addedJob3 = { project: { name: 'p3' }, activity: { title: 'a3' } };
+    const addedJob1 = {
+      project: { name: 'project_name_1', customer: { name: 'customer_name_1' } },
+      activity: { title: 'job_title_1', staffing_needed_from: '2018-01-10' },
+    };
+    const addedJob2 = {
+      project: { name: 'project_name_2', customer: { name: 'customer_name_2' } },
+      activity: { title: 'job_title_2', staffing_needed_from: '2018-02-20' },
+    };
+    const addedJob3 = {
+      project: { name: 'project_name_3', customer: { name: 'customer_name_3' } },
+      activity: { title: 'job_title_3', staffing_needed_from: '2018-03-30' },
+    };
 
-    const removedJobA = { project: { name: 'p4' }, activity: { title: 'a4' } };
-    const removedJobB = { project: { name: 'p5' }, activity: { title: 'a5' } };
+    const removedJobA = { project: { name: 'project_name_4' }, activity: { title: 'job_title_4' } };
+    const removedJobB = { project: { name: 'project_name_5' }, activity: { title: 'job_title_5' } };
 
     it('should compute the good rendering', () => {
       // given
@@ -25,14 +34,14 @@ describe('Unit | Infrastructure | Mailing | jobs-changed-email-template', () => 
         '<p>Il y a du nouveau du côté du <a href="https://jobs.octo.com">Job Board</a>.</p>' +
         '<p>3 nouvelle(s) mission(s) à staffer :' +
         '<ul>' +
-        '<li><bold>a1</bold> pour le projet p1</li>' +
-        '<li><bold>a2</bold> pour le projet p2</li>' +
-        '<li><bold>a3</bold> pour le projet p3</li>' +
+        '<li><bold>job_title_1</bold> pour le projet project_name_1 pour le client customer_name_1 à partir du 10/01/2018</li>' +
+        '<li><bold>job_title_2</bold> pour le projet project_name_2 pour le client customer_name_2 à partir du 20/02/2018</li>' +
+        '<li><bold>job_title_3</bold> pour le projet project_name_3 pour le client customer_name_3 à partir du 30/03/2018</li>' +
         '</ul></p>' +
         '<p>2 mission(s) retirée(s) :' +
         '<ul>' +
-        '<li><bold>a4</bold> pour le projet p4</li>' +
-        '<li><bold>a5</bold> pour le projet p5</li>' +
+        '<li><bold>job_title_4</bold> pour le projet project_name_4</li>' +
+        '<li><bold>job_title_5</bold> pour le projet project_name_5</li>' +
         '</ul>' +
         '</p>' +
         '<p>Pour ne plus recevoir de nouvelles du Job Board, il est possible de <a href="https://jobs.octo.com/#/unsubscribe">se désabonner du Job Board</a>.</p>';
@@ -54,8 +63,8 @@ describe('Unit | Infrastructure | Mailing | jobs-changed-email-template', () => 
         '<p>Il y a du nouveau du côté du <a href="https://jobs.octo.com">Job Board</a>.</p>' +
         '<p>2 mission(s) retirée(s) :' +
         '<ul>' +
-        '<li><bold>a4</bold> pour le projet p4</li>' +
-        '<li><bold>a5</bold> pour le projet p5</li>' +
+        '<li><bold>job_title_4</bold> pour le projet project_name_4</li>' +
+        '<li><bold>job_title_5</bold> pour le projet project_name_5</li>' +
         '</ul>' +
         '</p>' +
         '<p>Pour ne plus recevoir de nouvelles du Job Board, il est possible de <a href="https://jobs.octo.com/#/unsubscribe">se désabonner du Job Board</a>.</p>';
@@ -77,9 +86,9 @@ describe('Unit | Infrastructure | Mailing | jobs-changed-email-template', () => 
         '<p>Il y a du nouveau du côté du <a href="https://jobs.octo.com">Job Board</a>.</p>' +
         '<p>3 nouvelle(s) mission(s) à staffer :' +
         '<ul>' +
-        '<li><bold>a1</bold> pour le projet p1</li>' +
-        '<li><bold>a2</bold> pour le projet p2</li>' +
-        '<li><bold>a3</bold> pour le projet p3</li>' +
+        '<li><bold>job_title_1</bold> pour le projet project_name_1 pour le client customer_name_1 à partir du 10/01/2018</li>' +
+        '<li><bold>job_title_2</bold> pour le projet project_name_2 pour le client customer_name_2 à partir du 20/02/2018</li>' +
+        '<li><bold>job_title_3</bold> pour le projet project_name_3 pour le client customer_name_3 à partir du 30/03/2018</li>' +
         '</ul>' +
         '</p>' +
         '<p>Pour ne plus recevoir de nouvelles du Job Board, il est possible de <a href="https://jobs.octo.com/#/unsubscribe">se désabonner du Job Board</a>.</p>';
